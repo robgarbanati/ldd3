3f4a5ed19d0d55a4b6640ef2a80c6ecb00faf2cd
Rob Garbanati <robgarbanati@gmail.com>
2020-05-15 20:51:52 -0700

    added robs_scull

--- examples/misc-modules/hellop.c
+++ examples/misc-modules/hellop.c
@@ -1,6 +1,3 @@
-/*                                                     
- * $Id: hellop.c,v 1.4 2004/09/26 07:02:43 gregkh Exp $ 
- */                                                    
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -12,7 +9,7 @@ MODULE_LICENSE("Dual BSD/GPL");
  * are needed to make hello.c run properly even when      
  * your kernel has version support enabled                
  */                                                       
-                                                          
+
 
 /*
  * A couple of parameters that can be passed in: how many times we say
@@ -25,15 +22,15 @@ module_param(whom, charp, S_IRUGO);
 
 static int hello_init(void)
 {
-	int i;
-	for (i = 0; i < howmany; i++)
-		printk(KERN_ALERT "(%d) Hello, %s\n", i, whom);
-	return 0;
+    int i;
+    for (i = 0; i < howmany; i++)
+        printk(KERN_ALERT "(%d) Hello, %s\n", i, whom);
+    return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+    printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
---
+++ examples/robs_scull/scull.mod
@@ -0,0 +1,2 @@
+/home/rob/personal/ldd/examples/robs_scull/main.o /home/rob/personal/ldd/examples/robs_scull/pipe.o /home/rob/personal/ldd/examples/robs_scull/access.o
+
